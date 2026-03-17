591f46ac3b0b37a9f40013b1d430d7332b3a02f5
majoz-t <majozta@gmail.com>
2026-03-17 13:01:00 +0100

    initial commit

---
+++ src/main/java/com/labotigadelaterra/la_botiga_de_la_terra/LaBotigaDeLaTerraApplication.java
@@ -0,0 +1,13 @@
+package com.labotigadelaterra.la_botiga_de_la_terra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaBotigaDeLaTerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaBotigaDeLaTerraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/labotigadelaterra/la_botiga_de_la_terra/LaBotigaDeLaTerraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.labotigadelaterra.la_botiga_de_la_terra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaBotigaDeLaTerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
