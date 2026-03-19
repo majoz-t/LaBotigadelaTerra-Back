9aaf4c227b5e29fc06ea5832d94feb5f9ec89263
majoz-t <majozta@gmail.com>
2026-03-19 16:51:13 +0100

    feat: create  RegisterRequestDTO and create method for user registration

--- src/main/java/com/labotigadelaterra/la_botiga_de_la_terra/service/UserServiceImpl.java
+++ src/main/java/com/labotigadelaterra/la_botiga_de_la_terra/service/UserServiceImpl.java
@@ -2,7 +2,40 @@ package com.labotigadelaterra.la_botiga_de_la_terra.service;
 
 import org.springframework.stereotype.Service;
 
+import com.labotigadelaterra.la_botiga_de_la_terra.dto.request.RegisterRequestDTO;
+import com.labotigadelaterra.la_botiga_de_la_terra.entity.User;
+import com.labotigadelaterra.la_botiga_de_la_terra.entity.enums.Role;
+import com.labotigadelaterra.la_botiga_de_la_terra.repository.UserRepository;
+
 @Service
 public class UserServiceImpl implements UserService{
+
+    private final UserRepository userRepository;
+    
+    public UserServiceImpl (UserRepository userRepository){
+        this.userRepository= userRepository;
+    }
+
+    @Override
+    public User registerUser(RegisterRequestDTO dto) {
+       String email = dto.email().toLowerCase().trim();
+       if (userRepository.existsByEmail(email)){
+        throw new RuntimeException("El email ya está registrado");
+       }
+       User user = new User();
+       user.setName(dto.name().trim());
+       user.setEmail(email);
+       user.setPassword(dto.password());
+       user.setRole(Role.ROLE_PATIENT);
+
+       return userRepository.save(user);
+    }
+
+
+    // @Override
+    // public User findByEmail(String email) {
+    //     return userRepository.findByEmail(email)
+    //     .orElseThrow(()-> new RuntimeException("Usuario no encontrado"));
+    // }
 
 }
